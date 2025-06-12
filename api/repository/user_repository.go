10478cbbbb21ec9157f997931c267185c7604c86
EstebanFonseca <estebanfonsecadev@gmail.com>
2025-06-12 10:05:02 -0500

    creacion de repositorio con las operaciones

---
+++ api/repository/user_repository.go
@@ -0,0 +1,15 @@
+package repository
+
+import (
+	"context"
+	"api_users/api/models"
+)
+
+// UserRepository define la interfaz para las operaciones de usuarios.
+type UserRepository interface {
+	Create(ctx context.Context, user *models.User) error
+	List(ctx context.Context) ([]*models.User, error)
+	GetByID(ctx context.Context, id string) (*models.User, error)
+	Update(ctx context.Context, user *models.User) error
+	Delete(ctx context.Context, id string) error
+}
